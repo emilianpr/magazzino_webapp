0225f332f858983c50e8c28a14652d9adb6508d8
emilianpr <emilianprepelita107@gmail.com>
2025-10-23 09:56:28 +0200

    minor changes

---
+++ config_local.py
@@ -0,0 +1,12 @@
+# Database Configuration - NON commitare questo file su Git!
+DATABASE_CONFIG = {
+    'host': 'localhost',
+    'user': 'magazzino_webapp', 
+    'password': 'Emilian@21082007',
+    'database': 'magazzino_db'
+}
+
+FLASK_CONFIG = {
+    'secret_key': 'f3b1a67c9e8f4d2a85e37c1f9b7d4e6f2c1a5d8f9b3c4e7f0a1d2b3c4e5f6a7b',
+    'debug': False
+}
